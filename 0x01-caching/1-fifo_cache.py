98a159069c3b48fa96cf893b53dacbe3e3fd56ef
Roylouis-web <powellleroy488@gmail.com>
2023-07-25 14:56:27 +0000

    Task #1 and #2 completed

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bon/env python3
+
+"""
+    Module for a subclass called FIFOCache
+    that inherits from BaseCaching class
+"""
+
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+        A class calles FIFOCache that inherits from a
+        base class called BaseCaching and is a caching system
+    """
+
+    def __init__(self):
+        """
+            Overloads the __init__ method of the base class
+        """
+
+        super().__init__()
+
+    def put(self, key, item):
+        """
+            Assigns a new key/value pair to self.cache_data
+        """
+
+        if key and item:
+            cached_data = self.cache_data
+            cached_data.update({key: item})
+            keys = [k for k in cached_data]
+
+            if len(keys) > self.MAX_ITEMS:
+                del cached_data[keys[0]]
+                print(f"DISCARD: {keys[0]}")
+
+    def get(self, key):
+        """
+            returns the value in self.cache_data
+            linked to key
+        """
+
+        if key and self.cache_data[key]:
+            return self.cache_data[key]
+        return None
---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/envvpython3
+
+"""
+    Module for a class called LIFOCache that
+    inherits from the class BaseCaching
+"""
+
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+        A class called LIFOCache that inherits from a
+        base clas called BaseCaching and is a caching system
+    """
+
+    def __init__(self):
+        """
+            Overloads the __init_ method of the base class
+        """
+
+        super().__init__()
+
+    def put(self, key, item):
+        """
+            adds new key/value pairs to be cached in the
+            self.cache_data dictionary
+        """
+
+        if key and item:
+            cached_data = self.cache_data
+            cached_data.update({key: item})
+            keys = [k for k in cached_data]
+
+            if len(keys) > self.MAX_ITEMS:
+                del cached_data[keys[-1]]
+                print(f"DISCARD: {keys[-1]}")
+
+    def get(self, key):
+        """
+            returns the value of key that is linked to
+            self.cache_dat
+        """
+
+        if key and self.cache_data[key]:
+            return self.cache_data[key]
+        return None
