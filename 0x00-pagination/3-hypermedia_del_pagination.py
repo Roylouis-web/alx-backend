acfd299da23ed784c5bd0ee65eeceb6bec389584
Roylouis-web <powellleroy488@gmail.com>
2023-07-25 07:13:39 +0000

    All done

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -49,8 +49,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,
-                        page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
             returns a dictionary containing info about
             an index in a database
